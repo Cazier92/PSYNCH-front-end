37951f127b42b090e5b8788a427a7d690494c1dd
Yong Park <pyongho0306@gmail.com>
2023-02-04 11:40:06 -0800

    Basic structure done for login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,24 +1,29 @@
-import { useState } from 'react'
-import LoginForm from '../../components/LoginForm/LoginForm'
-import styles from './Login.module.css'
+import { useState } from "react";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import styles from "./Login.module.css";
 
-const LoginPage = props => {
-  const [message, setMessage] = useState([''])
+const LoginPage = (props) => {
+  const [message, setMessage] = useState([""]);
 
-  const updateMessage = msg => {
-    setMessage(msg)
-  }
+  const updateMessage = (msg) => {
+    setMessage(msg);
+  };
 
   return (
     <main className={styles.container}>
-      <h1>Log In</h1>
-      <p>{message}</p>
-      <LoginForm
-        handleSignupOrLogin={props.handleSignupOrLogin}
-        updateMessage={updateMessage}
-      />
+      <div className={styles.leftContainer}>
+        {/* <img src="./hands.jpeg" alt="" /> */}
+      </div>
+      <div className={styles.rightContainer}>
+        <h1>Log In</h1>
+        <p>{message}</p>
+        <LoginForm
+          handleSignupOrLogin={props.handleSignupOrLogin}
+          updateMessage={updateMessage}
+        />
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
