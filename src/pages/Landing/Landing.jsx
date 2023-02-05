a0ef96df8fe28b55a4b949028c1a129ded0d2f36
Yong Park <pyongho0306@gmail.com>
2023-02-05 00:53:41 -0800

    Fix navbar text bug

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,50 +1,45 @@
-import './Landing.css'
-import { Link } from 'react-router-dom'
+import "./Landing.css";
+import { Link } from "react-router-dom";
 
 const linkStyle = {
   textDecoration: "none",
-  color: "black"
-}
+  color: "black",
+};
 
 const Landing = ({ user }) => {
   return (
-    <main >
-      <section className='hero'>
-        <div className='hero-statement'>
-          <div className='statement-container'>
-            <p>THE SOCIAL NETWORK <br/> FOR <strong>EMOTIONS.</strong></p>
+    <main>
+      <section className="hero">
+        <div className="hero-statement">
+          <div className="statement-container">
+            <p className="firstChild">
+              THE SOCIAL NETWORK <br /> FOR <strong>EMOTIONS.</strong>
+            </p>
             <p>Experience the power real of connection. </p>
-            <p>Become part of the growing PSYNCH community and share your emotions with people from around the world!</p>
-            <button className='sign-up-btn'>
-              <Link to="/signup" style={ linkStyle }>
+            <p>
+              Become part of the growing PSYNCH community and share your
+              emotions with people from around the world!
+            </p>
+            <button className="sign-up-btn">
+              <Link to="/signup" style={linkStyle}>
                 SIGN UP
               </Link>
             </button>
           </div>
         </div>
-        <div className='hero-image'>
-          img
-        </div>
+        <div className="hero-image">img</div>
       </section>
-      <section className='main-content'>
-        <p>HOW ARE YOU FEELING?</p>
-        <div className='main-moods'>
-          <div>
-            Emotion
-          </div>
-          <div>
-            Emotion
-          </div>
-          <div>
-            Emotion
-          </div>
-          <div>
-            Emotion
-          </div>
+      <section className="main-content">
+        <p className="firstChild">HOW ARE YOU FEELING?</p>
+        <div className="main-moods">
+          <div>Emotion</div>
+          <div>Emotion</div>
+          <div>Emotion</div>
+          <div>Emotion</div>
         </div>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
