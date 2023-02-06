9d6f9537638dfcd6923ef341d6d52f85f6010546
Yong Park <pyongho0306@gmail.com>
2023-02-05 19:25:28 -0800

    Stub up state for post details

--- src/pages/PostDetails/PostDetails.jsx
+++ src/pages/PostDetails/PostDetails.jsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import styles from "./PostDetails.module.css";
+
+import * as postService from "../../services/emotionPostService";
+
 const PostDetails = (props) => {
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const data = await postService.show(id);
+      setPost(data);
+    };
+    fetchPost();
+  }, [id]);
+
+  console.log("Post State", post);
+
   return (
     <div>
       <h1>Post Details</h1>
