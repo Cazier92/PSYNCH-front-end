5589c615a7daafd3f993e73fe15b4e904813c79c
k-vernon <kvernon908@gmail.com>
2023-02-05 23:44:44 -0500

    add feed buttons

--- src/pages/MainFeed/MainFeed.jsx
+++ src/pages/MainFeed/MainFeed.jsx
@@ -2,20 +2,27 @@ import { Link } from 'react-router-dom'
 import './MainFeed.css'
 import PostList from '../PostList/PostList'
 
+import { useState } from 'react'
 
-// import { useState } from 'react'
 
 const MainFeed = ({posts}) => {
+  const [selected, setSelected] = useState()
   return (
     <>
       <div className='btns-container'>
-        <button className='global-btn'>
+        <button 
+          className={`global-btn ${selected ? "active" : "inactive"}`}
+          onClick={() => setSelected(!selected)}
+        >
           For You
         </button>
-        <button className='friends-btn'>
+        <button className={`friends-btn ${selected ? "active" : "inactive"}`}
+          onClick={() => setSelected(!selected)}
+        >
           Friends
         </button>
       </div>
+      {}
       <PostList posts={posts} />
       
     </>
