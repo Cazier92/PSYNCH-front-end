035f7b1378e35b160ad95e2fae1af6102b8eec35
Cazier92 <bryce.cazier@gmail.com>
2023-02-05 19:14:56 -0800

    complete SendFriendRequest, minus reciecevedRequest State

--- src/components/FriendRequests/SendFriendRequest/SendFriendRequest.jsx
+++ src/components/FriendRequests/SendFriendRequest/SendFriendRequest.jsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useInsertionEffect, useState } from 'react';
 
 
 import * as profileService from '../../../services/profileService'
@@ -16,29 +16,45 @@ const SendFriendRequest = (props) => {
     fetchProfile()
   }, [props.user.profile])
 
-  // console.log(userProfile.friendRequests.filter(element => element._id === userProfile._id))
-  // console.log(userProfile.friendRequests)
+  const [friendState, setFriendState] = useState('notFriends')
 
-  if (props.profile.friendRequests.includes(element => element._id === userProfile._id)) {
+  useEffect(() => {
+    if (props.profile.friendRequests.includes(element => element._id === userProfile._id)) {
+      setFriendState('pending')
+    } else if (props.profile.friends.some(element => element._id === userProfile._id)) {
+      setFriendState('friends')
+    // } else if (userProfile.friendRequests.some(element => element._id === props.profile._id)) {
+    //   setFriendState('receivedRequest')
+    } else if (props.profile._id === userProfile._id) {
+      setFriendState('user')
+    } else {
+
+    }
+  }, [])
+  
+  console.log(friendState)
+
+
+  if (friendState === 'pending') {
     return (
       <>
         <p>Friend Request Pending</p>
       </>
     )
-  } else if (props.profile.friends.some(element => element._id === userProfile._id)) {
+  } else if (friendState === 'friends') {
     return (
     <>
       <p>You and {props.profile.name} are friends!</p>
     </>
 
     )
-  } else if (userProfile.friendRequests?.some(element => element._id === userProfile._id)) {
+  } else if (friendState === 'recievedRequest') {
     return (
       <>
       <p>{props.profile.name} sent you a friend request!</p>
       </>
     )
-  } else if (props.profile._id === userProfile._id) {
+  } else if (friendState === 'user') {
     return (
       <>
       </>
