129d15a99a2a609162122c43d7999b86baaf862c
Yong Park <pyongho0306@gmail.com>
2023-02-06 09:17:22 -0800

    Fix minor bug for postDetails

--- src/App.jsx
+++ src/App.jsx
@@ -95,25 +95,24 @@ const App = () => {
             </ProtectedRoute>
           }
         />
-        <Route 
-          path="/profile/:id" 
+        <Route
+          path="/profile/:id"
           element={
-          <ProtectedRoute user={user}>
-            <Profile user={user}/>
-          </ProtectedRoute>
-      } />
-        <Route 
-          path="/profile/requests" 
+            <ProtectedRoute user={user}>
+              <Profile user={user} />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile/requests"
           element={
-          <ProtectedRoute user={user}>
-            <PendingRequests user={user}/>
-          </ProtectedRoute>
-      } />
-      <Route 
-          path="/main-feed" 
-          element={
-          <MainFeed posts={posts} />
-        } />
+            <ProtectedRoute user={user}>
+              <PendingRequests user={user} />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/main-feed" element={<MainFeed posts={posts} />} />
+        <Route path="/posts/:id" element={<PostDetails posts={posts} />} />
       </Routes>
     </>
   );
