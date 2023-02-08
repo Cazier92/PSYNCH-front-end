84737cad9827f26016f278ab9a770e4897e1b711
Cazier92 <bryce.cazier@gmail.com>
2023-02-07 17:42:06 -0800

    update profile page to only render private posts if user matches the profile or is friends with the profile owner

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -8,19 +8,28 @@ import PostList from '../../components/PostList/PostList';
 const Profile = ({user}) => {
   const {id} = useParams()
   const [profile, setProfile] = useState(null)
+  const [viewPrivate, setViewPrivate] = useState(null)
 
   useEffect(() => {
     const fetchProfile = async () => {
       const profileData = await profileService.show(id)
       setProfile(profileData)
-      // console.log('profile data:', profileData)
-      // console.log('profile:', profile)
     }
     fetchProfile()
   }, [id])
-  
-  // const sortedPosts = profile.emotionPosts.sort({createdAt: 'desc'})
-  // console.log(profile.emotionPosts.sort((a, b) => a - b))
+
+  const privatePosts = () => {
+    if (user.profile === profile._id) {
+      return profile.emotionPosts
+    }
+    else if (profile.friends.some(friend => friend._id === user.profile)) {
+      return profile.emotionPosts
+    }
+    else {
+      return profile.emotionPosts.filter(post => post.public === true)
+    }
+  }
+
 
 
   if (!profile) return <h1>Loading</h1>
@@ -29,7 +38,7 @@ const Profile = ({user}) => {
       <h1>{profile.name}</h1>
       <img src={profile.avatar} alt="" />
       <SendFriendRequest profile={profile} user={user}/>
-      <PostList posts={profile.emotionPosts} user={user}/>
+      <PostList posts={privatePosts()} user={user}/>
     </>
   );
 }
