9e676577d21d73952f2ec499e30b6bf13d5ce7b7
k-vernon <kvernon908@gmail.com>
2023-02-09 03:20:25 -0500

    section out friends

--- src/components/FriendList/Friend.jsx
+++ src/components/FriendList/Friend.jsx
@@ -7,19 +7,19 @@ const Friend = ({friend}) => {
   return ( 
     <>
     <div className='friend-card'>
-      <div className='friend-avatar-container'>
+      <div className='friend-container1'>
         <img
           className="friend-avatar"
           src={friend.photo}
           alt="profile img"
         />
       </div>
-      <div className='friend-info-container'>
+      <div className='friend-container2'>
         <div className='friend-title-container'>
           <p className='friend-name'>{friend.name}</p>
           <p><em>Friends</em></p>
         </div>
-        <div className='friend-status-container'>
+        <div className='friend-container3'>
           <p className='friend-status'>{friend.currentStatus}</p>
         </div>
 
