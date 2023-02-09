f2111cfbfe4fef95a4693bbdf0262d3a26c0355b
k-vernon <kvernon908@gmail.com>
2023-02-09 14:15:35 -0500

    change nav

--- src/App.jsx
+++ src/App.jsx
@@ -199,7 +199,7 @@ const App = () => {
 
   return (
     <>
-      <NavBar user={user} handleLogout={handleLogout} />
+      <NavBar user={user}  handleLogout={handleLogout} />
       <Routes>
         <Route path="/" element={<Landing user={user} />} />
         <Route
