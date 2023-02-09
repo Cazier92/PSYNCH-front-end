8896bb421799d30c9afb8691593d972bae3c4101
Cazier92 <bryce.cazier@gmail.com>
2023-02-09 15:01:52 -0800

    basic styling for Chat complete

--- src/pages/DirectMessages/Chat.jsx
+++ src/pages/DirectMessages/Chat.jsx
@@ -6,6 +6,8 @@ import * as directMessagesService from '../../services/directMessagesService'
 
 import SendMessage from "../../components/DirectMessages/SendMessage";
 
+import './Chat.css'
+
 
 const Chat = ({handleCreateNotification, newNotificationId, user}) => {
   const {conversationId} = useParams()
@@ -52,25 +54,47 @@ const Chat = ({handleCreateNotification, newNotificationId, user}) => {
   return ( 
     <>
     <MainFeedBars user={user}/>
-    <h3>Chat Members:</h3>
-    {conversation.members?.map(member => {
-      return (
-        <h5>{member.name}</h5>
-      )
-    })}
-    <h3>Messages:</h3>
-    {messages?.map(message => {
-      return (
-        <>
-        <p>{message.content}</p>
-        <h6>{message.author.name}</h6>
-        </>
-      )
-    })}
-    <div>
-      <SendMessage conversationId={conversationId} handleSendMessage={handleSendMessage} handleCreateNotification={handleCreateNotification} newNotificationId={newNotificationId} profile={profile} user={user} setProfile={setProfile} conversation={conversation} messages={messages}/>
 
-    </div>
+      <div className="messages-div">
+        <div className="header-div">
+          <h3 className="messages-head">Chat Members:</h3>
+          {conversation.members?.map(member => {
+            return (
+              <h5 className="messages-head">{member.name}</h5>
+            )
+          })}
+          {/* <h3 className="messages-head">Messages:</h3> */}
+        </div>
+        {messages?.map(message => {
+          if (message.author._id === user.profile) {
+            return (
+              <>
+              <div className="user-message">
+                <p className="user-message-content">{message.content}</p>
+                <h6 className="user-name">{message.author.name}</h6>
+              </div>
+              </>
+            )
+          } else {
+            return (
+              <>
+              <div className="friend-message">
+                <p className="friend-message-content">{message.content}</p>
+                <h6 className="friend-name">{message.author.name}</h6>
+              </div>
+              </>
+            )
+
+          }
+        })}
+        <div className="send-messages">
+          <SendMessage conversationId={conversationId} handleSendMessage={handleSendMessage} handleCreateNotification={handleCreateNotification} newNotificationId={newNotificationId} profile={profile} user={user} setProfile={setProfile} conversation={conversation} messages={messages}/>
+
+        </div>
+
+      </div>
+
+
     </>
   );
 }
