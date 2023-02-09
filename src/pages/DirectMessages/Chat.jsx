1a5ed3570a02d4e263d9979d50edf544697323c1
Cazier92 <bryce.cazier@gmail.com>
2023-02-08 21:05:15 -0800

    basic layout of Chat page set up

--- src/pages/DirectMessages/Chat.jsx
+++ src/pages/DirectMessages/Chat.jsx
@@ -16,17 +16,31 @@ const Chat = (props) => {
       // console.log(conversationData)
     }
     fetchConversation()
-  }, [])
+  }, [conversationId])
 
-  console.log(conversationId)
-  console.log(conversation.members)
+  // console.log(conversationId)
+  // console.log(conversation.members[0].name)
 
 
   return ( 
     <>
-
+    <h3>Chat Members:</h3>
+    {conversation.members?.map(member => {
+      return (
+        <h5>{member.name}</h5>
+      )
+    })}
+    <h3>Messages:</h3>
+    {conversation.messages?.map(message => {
+      return (
+        <>
+        <p>{message.content}</p>
+        <h6>{message.author}</h6>
+        </>
+      )
+    })}
     </>
-   );
+  );
 }
- 
+
 export default Chat;
