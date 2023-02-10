858e5961fd3786c6eaaa1b3ddda3821e7bbbf849
Cazier92 <bryce.cazier@gmail.com>
2023-02-09 22:33:56 -0800

    modify chat

--- src/pages/DirectMessages/Chat.jsx
+++ src/pages/DirectMessages/Chat.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import MainFeedBars from "../../components/MainFeedBars/MainFeedBars";
 
 import * as directMessagesService from '../../services/directMessagesService'
+import * as profileService from '../../services/profileService'
 
 import SendMessage from "../../components/DirectMessages/SendMessage";
 
@@ -13,6 +14,9 @@ const Chat = ({handleCreateNotification, newNotificationId, user, userProfile, a
   const {conversationId} = useParams()
   const [conversation, setConversation] = useState([])
   const [messages, setMessages] = useState([])
+  const [newMessage, setNewMessage] = useState()
+  // const [newNewMessage, setNewNewMessage] = useState({})
+  // const [newMessageAuthor, setNewMessageAuthor] = useState({})
 
   const [profile, setProfile] = useState({})
 
@@ -40,8 +44,61 @@ const Chat = ({handleCreateNotification, newNotificationId, user, userProfile, a
 
   const handleSendMessage = async (id, messageData) => {
     const newMessage = await directMessagesService.sendMessage(conversationId, messageData)
-    setConversation({...conversation, messages: [...conversation.messages, newMessage]})
+    setNewMessage(newMessage)
+    // setConversation({...conversation, messages: [...conversation.messages, newMessage,]})
+    // console.log('NEW MESSAGE:', newMessage)
   }
+  
+  // useEffect( () => {
+  //   const fetchProfile = async () => {
+  //     const data = await profileService.getAllProfiles();
+  //     setNewMessageAuthor(data.filter((profile) => profile._id === newMessage.author)[0]);
+  //   };
+  //   fetchProfile();
+  //   const newMessageFunc = async () => {
+  //     await fetchProfile()
+  //     .then(profile => {
+  //       setNewNewMessage({...newMessage, author: newMessageAuthor})
+  //       console.log(newMessage)
+  //     })
+  //   }
+  //   newMessageFunc()
+  //   const newConversationFunc = async () => {
+  //     await fetchProfile()
+  //     await newMessageFunc()
+  //     .then(message => {
+  //       if (newNewMessage) {
+
+  //         setConversation({...conversation, messages: [...conversation.messages, newNewMessage,]})
+  //       }
+
+  //     })
+  //   }
+  //   newConversationFunc()
+  // }, [conversation, messages, newMessage]);
+  
+
+
+  // console.log('NEWNEW MESSAGE', newNewMessage)
+  let newMessagesArr = []
+
+  const newMessageRender = () => {
+    if (newMessage) {
+      // return (
+      //   <>
+      //     <div className="user-message">
+      //       <p className="user-message-content">{newMessage.content}</p>
+      //       <h6 className="user-name">{user.name}</h6>
+      //     </div>
+      //   </>
+      // )
+      newMessagesArr.push({
+        content: newMessage.content,
+        author: user.name
+      })
+    }
+  }
+  newMessageRender()
 
   return ( 
     <>
@@ -52,22 +109,13 @@ const Chat = ({handleCreateNotification, newNotificationId, user, userProfile, a
           <h3 className="messages-head">Chat Members:</h3>
           {conversation.members?.map(member => {
             return (
-              <h5 className="messages-head">{member.name}</h5>
+              <h5 className="messages-head" key={member.name}>{member.name}</h5>
             )
           })}
           {/* <h3 className="messages-head">Messages:</h3> */}
         </div>
         {messages?.map(message => {
-          if (message.author._id === user.profile) {
-            return (
-              <>
-              <div className="user-message">
-                <p className="user-message-content">{message.content}</p>
-                <h6 className="user-name">{message.author.name}</h6>
-              </div>
-              </>
-            )
-          } else {
+          if (message.author._id !== user.profile) {
             return (
               <>
               <div className="friend-message">
@@ -76,9 +124,34 @@ const Chat = ({handleCreateNotification, newNotificationId, user, userProfile, a
               </div>
               </>
             )
+          } else if (message.author._id === user.profile) {
+            return (
+              <>
+              <div className="user-message">
+                <p className="user-message-content">{message.content}</p>
+                <h6 className="user-name">{message.author.name}</h6>
+              </div>
+              </>
+            )
 
+          } else {
+            <>
+            <div className="user-message">
+              <p className="user-message-content">{message.content}</p>
+              <h6 className="user-name">{message.author.name}</h6>
+            </div>
+            </>
           }
         })}
+        {newMessagesArr.map(message => {
+          return (
+            <div className="user-message">
+            <p className="user-message-content">{message.content}</p>
+            <h6 className="user-name">{message.author}</h6>
+          </div>
+          )
+        })}
+          {/* {newMessageRender()} */}
         <div className="send-messages">
           <SendMessage conversationId={conversationId} handleSendMessage={handleSendMessage} handleCreateNotification={handleCreateNotification} newNotificationId={newNotificationId} profile={profile} user={user} setProfile={setProfile} conversation={conversation} messages={messages}/>
 
